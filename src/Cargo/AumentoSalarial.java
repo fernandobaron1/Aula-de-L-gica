a03b64966f2d306c6c282870d49499e27cef086d
Aluno <Aluno@SN-279237>
2019-06-27 19:10:04 -0300

    Segunda Atualização

--- src/Cargo/AumentoSalarial.java
+++ src/Cargo/AumentoSalarial.java
@@ -45,6 +45,44 @@ public class AumentoSalarial {
         
         return novoSalario;
     }
+    private double calcEngenheiro() {
+        
+        int tempo = this.func.getTempServico(); 
+        double percentual = 0;
+        
+        if (tempo >= 1 & tempo < 2) {
+        percentual = 10;
+    } else if (tempo <= 3) {
+            percentual = 20;
+    } else {
+            percentual = 25;
+            }
+    double salario = this.func.getSalario();
+    
+    double novoSalario = (salario * percentual) / 100;
+    novoSalario = salario + novoSalario;
+        
+        return novoSalario;
+    }
+    private double calcTecnico() {
+        
+        int tempo = this.func.getTempServico(); 
+        double percentual = 0;
+        
+        if (tempo >= 1 & tempo < 2) {
+        percentual = 10;
+    } else if (tempo <= 3) {
+            percentual = 20;
+    } else {
+            percentual = 25;
+            }
+    double salario = this.func.getSalario();
+    
+    double novoSalario = (salario * percentual) / 100;
+    novoSalario = salario + novoSalario;
+        
+        return novoSalario;
+    }
     private double calcGeral() {
         int tempo = this.func.getTempServico();
         double percentual = 0;
